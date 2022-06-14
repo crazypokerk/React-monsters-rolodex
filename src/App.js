98775557d64d9ddc75f1498949365b086f9b66f7
crazypokerk <downnn@live.com>
2022-06-14 15:55:04 +0800

    basic complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react'
+
+import CardList from './conmponents/card-list/card-list.component'
+import SearchBox from './conmponents/search-box/search-box.component'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  // [2]
+  onSearchChange = (event) => {
+    // console.log(`event.target.value:${event.target.value}`)
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(resonse => resonse.json())
+      .then(users => this.setState(
+        () => {
+          return { monsters: users }
+        }
+      ))
+  }
+
+  render() {
+    // [1]
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filterMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder='search monsters'
+          className='monster-search-box'
+        />
+
+        <CardList monsters={filterMonsters} className="card-list" />
+      </div >
+    );
+  }
 }
 
 export default App;
